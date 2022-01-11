351ba53dd32c0a681c41aa9f5dc44b58726f23a1
Rafał Pamięta <r.pamieta@gmail.com>
2022-01-11 18:53:53 +0100

    added site

---
+++ js/slick.js
@@ -0,0 +1,22 @@
+$(".team-carousel").slick({
+	autoplay: true,
+	arrows: false,
+	mobileFirst: true,
+	infinite: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	responsive: [
+		{
+			breakpoint: 767,
+			settings: {
+				slidesToShow: 2,
+			},
+		},
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 3,
+			},
+		},
+	],
+});
